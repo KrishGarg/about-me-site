05880ed95382748d8bf509c0e8260cc649575f5e
Krish Garg <krishgarg6306@gmail.com>
2021-09-10 23:05:04 +0530

    converted from plain html+css to gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,43 @@ import React from "react";
 // Components
 import Layout from "../components/Layout";
 
+// Styles
+import {
+  greet,
+  greetH,
+  greetP,
+  btns,
+  btn,
+} from "../styles/IndexPage.module.scss";
+
 const IndexPage = () => {
   return (
-    <div>
-      <Layout>hi</Layout>
-    </div>
+    <Layout>
+      <main>
+        <section>
+          <div className={greet}>
+            <h2 className={greetH}>
+              Hi &#128075;
+              <br />I am Krish Garg.
+            </h2>
+            <p className={greetP}>
+              I am a learning developer, currently very inspired and enlightened
+              with web development.
+            </p>
+            <div className={btns}>
+              <a
+                rel="noreferrer"
+                href="https://github.com/KrishGarg"
+                target="_blank"
+                className={btn}
+              >
+                Github
+              </a>
+            </div>
+          </div>
+        </section>
+      </main>
+    </Layout>
   );
 };
 
