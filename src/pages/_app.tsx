cafbf92e1fdf180671143a4b283131cdee741867
Krish Garg <krishgarg6306@gmail.com>
2022-03-26 02:02:58 +0530

    ✨ made loading page toggleable

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,15 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { FC, useEffect, useState } from "react";
+import { FC, useEffect, useState, useRef } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import type { AnimationProps } from "framer-motion/types/motion/types";
+import { RecoilRoot, useRecoilValue } from "recoil";
+import LoadingBar, { LoadingBarRef } from "react-top-loading-bar";
 
 import Navbar from "@/components/Navbar";
 import Loading from "@/components/Loading";
+
+import { loading as loadingState } from "@/lib/state";
 
 const pageVariant: AnimationProps["variants"] = {
   initial: {
@@ -23,10 +27,27 @@ const pageVariant: AnimationProps["variants"] = {
   },
 };
 
+const Page: FC = ({ children }) => {
+  return (
+    <motion.div
+      variants={pageVariant}
+      initial="initial"
+      animate="animate"
+      exit="exit"
+    >
+      {children}
+    </motion.div>
+  );
+};
+
 const App: FC<AppProps> = ({ Component, pageProps, router }) => {
   const [isPageChanging, setIsPageChanging] = useState(false);
   const [isLoadingAnimationDone, setIsLoadingAnimationDone] = useState(true);
   const [lastRoute, setLastRoute] = useState("");
+
+  const loading = useRecoilValue(loadingState);
+
+  const loadingRef = useRef<null | LoadingBarRef>(null);
 
   useEffect(() => {
     // watching router just for the edge case of router taking longer than loading animation
@@ -40,10 +61,13 @@ const App: FC<AppProps> = ({ Component, pageProps, router }) => {
       setLastRoute(currentRoute);
       setIsPageChanging(true);
       setIsLoadingAnimationDone(false);
+      (loadingRef.current as any)?.continuousStart();
     };
 
     const handleRouteEnd = () => {
       setIsPageChanging(false);
+      if (!loading) setIsLoadingAnimationDone(true);
+      loadingRef.current?.complete();
     };
 
     router.events.on("routeChangeStart", handleRouteStart);
@@ -54,29 +78,30 @@ const App: FC<AppProps> = ({ Component, pageProps, router }) => {
       router.events.off("routeChangeComplete", handleRouteEnd);
       router.events.off("routeChangeError", handleRouteEnd);
     };
-  }, [router, lastRoute]);
+  }, [router, lastRoute, loading]);
 
   return (
     <>
+      {!loading && <LoadingBar color="#232b2b" height={4} ref={loadingRef} />}
       <Navbar />
       <div className="mx-2">
         <AnimatePresence exitBeforeEnter={true}>
-          {!isPageChanging && isLoadingAnimationDone ? (
-            <motion.div
-              variants={pageVariant}
-              initial="initial"
-              animate="animate"
-              exit="exit"
-              key={router.route}
-            >
-              <Component {...pageProps} />
-            </motion.div>
+          {loading ? (
+            !isPageChanging && isLoadingAnimationDone ? (
+              <Page key={router.route}>
+                <Component {...pageProps} />
+              </Page>
+            ) : (
+              <Loading
+                key="loading"
+                setIsLoadingAnimationDone={setIsLoadingAnimationDone}
+                howLong={2000}
+              />
+            )
           ) : (
-            <Loading
-              key="loading"
-              setIsLoadingAnimationDone={setIsLoadingAnimationDone}
-              howLong={2000}
-            />
+            <Page key={router.route}>
+              <Component {...pageProps} />
+            </Page>
           )}
         </AnimatePresence>
       </div>
@@ -84,4 +109,12 @@ const App: FC<AppProps> = ({ Component, pageProps, router }) => {
   );
 };
 
-export default App;
+const ToExportApp: FC<AppProps> = (props) => {
+  return (
+    <RecoilRoot>
+      <App {...props} />
+    </RecoilRoot>
+  );
+};
+
+export default ToExportApp;
