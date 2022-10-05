36d428a8b1a3e76828be7fcb32ec365a09007fd8
Krish Garg <krishgarg6306@gmail.com>
2022-10-05 18:33:54 +0530

    🔥 sorry I just can't make up my mind

--- src/hooks/useIsMobile.ts
+++
@@ -1,7 +0,0 @@
-import { useMedia } from "react-use";
-
-export const CHECK_IF_MOBILE_QUERY = "(max-width: 768px)";
-
-const useIsMobile = () => useMedia(CHECK_IF_MOBILE_QUERY, false);
-
-export default useIsMobile;
