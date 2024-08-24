89a3b2abd40eddd95484f1d5776a2d11fa50b388
Krish Garg <krishgarg6306@gmail.com>
2024-08-24 19:10:10 +0530

    starting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,15 @@
+import Wavy from "@/components/Wavy";
+import Name from "@/components/Name";
+
 const Home = () => {
-  return <div>Hello World</div>;
+  return (
+    <section className="relative h-[50vh] bg-teal-400">
+      <div className="flex justify-center h-[50%] w-full items-center">
+        <Name />
+      </div>
+      <Wavy />
+    </section>
+  );
 };
 
 export default Home;
---
+++ src/components/Name.tsx
@@ -0,0 +1,12 @@
+const Name = () => {
+  return (
+    <div className="absolute text-slate-800 m-4 drop-shadow-md">
+      <span className="text-5xl font-bold">K</span>
+      <span className="text-3xl">rish</span>{" "}
+      <span className="text-5xl font-bold">G</span>
+      <span className="text-3xl">arg</span>
+    </div>
+  );
+};
+
+export default Name;
